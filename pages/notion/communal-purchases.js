d076624247d66bfef424d439f8d1dc55dde9cbfb
lil-j <jake@krunchy.net>
2022-10-27 16:26:05 -0700

    mobile

--- pages/notion/communal-purchases.js
+++ pages/notion/communal-purchases.js
@@ -2,7 +2,7 @@ import Log from "../communal-purchases/log";
 import Pay from "../communal-purchases/pay";
 
 export default function CommunalPurchases() {
-    return <div className="grid grid-cols-2">
+    return <div className="grid grid-cols-1 md:grid-cols-2">
         <Log hideBack/>
         <Pay hideBack/>
     </div>
